09bbfb5b1519330f95fb268c5711ed47da693be7
Min Kim <mk4555@nyu.edu>
2018-02-01 20:35:19 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,7 +19,7 @@ class StudentsController < ApplicationController
 
   def update
     @student = Student.find(params[:id])
-    @student.update()
+    @student.update(student_params(:first_name, :last_name))
   end
   private
 
