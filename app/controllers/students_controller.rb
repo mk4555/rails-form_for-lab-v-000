677f98be1d5804bd8b63abd32ccd8750a30020de
Min Kim <mk4555@nyu.edu>
2018-02-01 20:10:52 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,14 @@
 class StudentsController < ApplicationController
+  def new
+  end
+
+  def create
+    @student = Student.new()
+  end
+
+  private
+
+  def student_params(*args)
+    
+  end
 end
