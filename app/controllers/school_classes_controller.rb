c6157605216e76f811f3b5d727f5563e4c739a90
Min Kim <mk4555@nyu.edu>
2018-02-01 20:01:08 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,9 +1,21 @@
 class SchoolClassesController < ApplicationController
   def create
-
+    @post = Post.create(params[:])
   end
 
   def new
-    
+
+  end
+
+  private
+
+
+    # We pass the permitted fields in as *args;
+    # this keeps `post_params` pretty dry while
+    # still allowing slightly different behavior
+    # depending on the controller action
+    def post_params(*args)
+      params.require(:post).permit(*args)
+    end
   end
 end
