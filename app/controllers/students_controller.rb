150e60ac95524e8be16d0840c40ca83607b3968d
Min Kim <mk4555@nyu.edu>
2018-02-01 20:35:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,6 +22,7 @@ class StudentsController < ApplicationController
     @student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
+  
   private
 
   def student_params(*args)
